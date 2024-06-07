d15a0fcdb1d0f6e43fee7c3e7e22448c4ca5ab63
Josh Burkart <jburkart@gmail.com>
2024-06-07 12:05:57 -0700

    Set CORS headers in Vite config

--- tsunami/vite.config.js
+++ tsunami/vite.config.js
@@ -1,5 +1,21 @@
-export default {
+import { defineConfig } from "vite";
+
+const viteHeaderPlugin = {
+    name: 'add headers',
+    configureServer: (server) => {
+        server.middlewares.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET");
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            next(); dddd
+        });
+    }
+};
+
+export default defineConfig({
     build: {
         target: 'esnext'
-    }
-}
+    },
+    plugins: [viteHeaderPlugin],
+});
