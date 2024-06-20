771052b0ec59f2f820c50ed08b0e29f615ed5be4
Josh Burkart <jburkart@gmail.com>
2024-06-20 10:50:22 -0700

    Disable on mobile

--- tsunami/main.js
+++ tsunami/main.js
@@ -1,5 +1,15 @@
 import init, { initThreadPool, run } from './pkg/tsunami.js';
 
-await init();
-await initThreadPool(navigator.hardwareConcurrency);
-await run();
+var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+if (width < 700) {
+    document.getElementById('canvas').style.display = 'none';
+    document.getElementsByTagName('body')[0].style.padding = '20';
+    document.getElementById('info').style.display = 'block';
+} else {
+    await init();
+    await initThreadPool(navigator.hardwareConcurrency);
+    await run();
+}
+
